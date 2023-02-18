010bcfa88dc827a57fb73f82dd9046f72b784104
Sefakor19 <serphakor19@gmail.com>
2023-02-17 16:37:11 -0800

    Positive or Negative Task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  *
- * main - function to generate a random number
+ * main - main function to generate a random number
  *
  * Return: Always 0 (Success)
  *
@@ -19,17 +19,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
+	
 	else if (n == 0)
-	{
+	
 		printf("%d is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
 
+	else if (n < 0)
+	
+		printf("%d is negative\n", n);
+	return (0);
 }
