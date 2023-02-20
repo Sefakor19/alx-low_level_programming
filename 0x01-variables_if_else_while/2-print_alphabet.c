f779f0ec6f60f1f309715f7204451989856ac71f
Sefakor19 <serphakor19@gmail.com>
2023-02-19 16:46:24 -0800

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /**
- * main - a program that prints an alphabet in lower case, followed by a new line
- *
- * Return: Always 0 (Success)
- */
+* main - a program that prints an alphabet in lower case
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	int ch = 'a';
+
 
 	while (ch <= 'z')
 	{
