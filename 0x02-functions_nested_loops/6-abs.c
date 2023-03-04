26c519172d9b913b348cb9a94b8e27eb285e13e8
Sefakor19 <serphakor19@gmail.com>
2023-03-04 12:03:33 -0800

    Task_5-sign.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _abs(int t) - Computes the absolute value of an integer
+ * _abs - Computes the absolute value of an integer
  * Description: The integer to be computed
  * Return: The absolute value
  */
-int _abs(int t)
+int _abs(int)
 {
 	if (t >= 0)
 		return (t);
