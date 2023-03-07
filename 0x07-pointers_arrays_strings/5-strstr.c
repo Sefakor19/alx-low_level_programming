30eeeec903b88f2e88261086df8db85981fe0f76
Sefakor19 <serphakor19@gmail.com>
2023-03-06 17:50:49 -0800

    Task_5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check description
+ * @haystack: input
+ * @needle: input
+ * Description: The function locates a substring
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
